016de566024035724a386f51f7c2430e42c13689
Henning Hraban Ramm <hraban@fiee.net>
2010-07-07 22:27:02 +0200

    Translate app title in admin

---
+++ project_name/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from django.utils.translation import ugettext_lazy as _
+
+verbose_name = _(u'project_name')
+verbose_name_plural = _(u'project_names')
+
+
